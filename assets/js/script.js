ccedb79b0cb44986ed7a29456a83006f78ebf7fb
Daniel Abraham <dtmthinktank@hotmail.co.uk>
2024-06-13 05:23:42 +0000

    feat:added event listeners to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,17 @@ const fruits = [
     'grapes',
     'lemon'
 ]
+
+let gameSequence = [];
+let usersequence = [];
+let score = 0;
+
+document.addEventListener('DOMContentLoaded', () => {
+    const itemGrid = document.getElementById('item-grid');
+    const currentItemBox = document.getElementById('current-item');
+    const sequenceCounter = document.getElementById('sequence-counter');
+    const startButton = document.getElementById('start-button');
+    const resetButton = document.getElementById('reset-button')
+
+
+})
