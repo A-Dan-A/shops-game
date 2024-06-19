a23616c48a6e67a9bd9fb401ab33d0f8e2b12851
Daniel Abraham <dtmthinktank@hotmail.co.uk>
2024-06-19 14:50:59 +0000

    feat:added random item function

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,6 +34,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
     resetButton.addEventListener('click', resetGame);
 
-    
+    function startGame() {
+        startButton.style.display = 'none';
+        resetButton.style.display = 'block';
+        gameSequence = [];
+        userSequence = [];
+        score = 0;
+        sequenceCounter.textContent = 'Sequence Length: 0';
+        addRandomItemToSequence();
+    }
+
+    function resetGame() {
+        gameSequence = [];
+        userSequence = [];
+        score = 0;
+        sequenceCounter.textContent = 'Sequence Length: 0';
+        currentItemBox.innerHTML = '';
+        startButton.style.display = 'block';
+        resetButton.style.display = 'none';
+    }
+
+    function addRandomItemToSequence() {
+        const randomItem = items[Math.floor(Math.random() * items={.length})];
+        gameSequence.push(randomItem);
+        displayItem(randomItem);
+        updateCounter();
+        userSequence = [];
+    }
 
 })
